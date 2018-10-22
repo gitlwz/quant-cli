aad644b593a4df0b0bf7a8af5b4f1928bef8e6f5
liuwenzhu <547485261@qq.com>
2018-10-22 17:28:58 +0800

    add

---
+++ README.md
@@ -0,0 +1,14 @@
+用于快熟构建量投前端框架的脚手架工具。
+
+**安装**
+先决条件：[Node.js](https://nodejs.org/en/)（> = 6.x，8.x首选），npm版本3+和[Git](https://git-scm.com/)。
+
+> $ npm install -g quant-cli
+
+**用法**
+
+> $ quant init <project-name>
+
+例：
+
+> $ vue init my-project
---
+++ index.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const program = require('commander');
+const download = require('download-git-repo');
+const handlebars = require('handlebars');
+const inquirer = require('inquirer');
+const ora = require('ora');
+const chalk = require('chalk');
+const symbols = require('log-symbols');
+program.version('1.0.0', '-v, --version')
+    .command('init [name]')
+    .action((name) => {
+        name = name || "quant-project"
+        if(!fs.existsSync(name)){
+            inquirer.prompt([
+                {
+                    name: 'title',
+                    message: '请输入项目标题',
+                    default:"量投",
+                },{
+                    name: 'description',
+                    message: '请输入项目描述',
+                    default:"description"
+                },
+                {
+                    name: 'author',
+                    message: '请输入作者名称',
+                    default:"author"
+                },{
+                    type:"confirm",
+                    name: 'isTop',
+                    message: '导航栏是否在顶部',
+                    default:true,
+                },{
+                    name: 'version',
+                    message: '请输入项目版本',
+                    default:"1.0.0",
+                }
+            ]).then((answers) => {
+                const spinner = ora('正在下载模板...');
+                spinner.start();
+                download('gitlwz/new-quant', name, (err) => {
+                    if(err){
+                        spinner.fail();
+                        console.log(symbols.error, chalk.red(err),err);
+                    }else{
+                        spinner.succeed();
+                        const fileName = `${name}/package.json`;
+                        const configName = `${name}/src/common/config.js`;
+                        const meta = {
+                            name,
+                            description: answers.description,
+                            author: answers.author,
+                            version: answers.version
+                        }
+                        const config = {
+                            isTop:answers.isTop,
+                            title: answers.title,
+                        }
+                        if(fs.existsSync(fileName)){
+                            const content = fs.readFileSync(fileName).toString();
+                            const result = handlebars.compile(content)(meta);
+                            fs.writeFileSync(fileName, result);
+                        }
+                        if(fs.existsSync(configName)){
+                            const content = fs.readFileSync(configName).toString();
+                            const result = handlebars.compile(content)(config);
+                            fs.writeFileSync(configName, result);
+                        }
+                        console.log(symbols.success, chalk.green('项目初始化完成'));
+                    }
+                })
+            })
+        }else{
+            // 错误提示项目已存在，避免覆盖原有项目
+            console.log(symbols.error, chalk.red('项目已存在'));
+        }
+    })
+program.parse(process.argv);
