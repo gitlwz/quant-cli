2fbde95e6d12ec066309329e15c30ed8643e233f
liuwenzhu <547485261@qq.com>
2018-12-20 16:06:36 +0800

    add

--- index.js
+++ index.js
@@ -7,43 +7,52 @@ const inquirer = require('inquirer');
 const ora = require('ora');
 const chalk = require('chalk');
 const symbols = require('log-symbols');
-program.version('1.0.0', '-v, --version')
+program.version('1.1.0', '-v, --version')
     .command('init [name]')
     .action((name) => {
         name = name || "quant-project"
-        if(!fs.existsSync(name)){
+        if (!fs.existsSync(name)) {
             inquirer.prompt([
                 {
                     name: 'title',
                     message: '请输入项目标题',
-                    default:"量投",
-                },{
+                    default: "量投",
+                }, {
                     name: 'description',
                     message: '请输入项目描述',
-                    default:"description"
+                    default: "description"
                 },
                 {
                     name: 'author',
                     message: '请输入作者名称',
-                    default:"author"
-                },{
-                    type:"confirm",
+                    default: "author"
+                }, {
+                    type: "confirm",
                     name: 'isTop',
                     message: '导航栏是在顶部Y|| 侧边N',
-                    default:true,
-                },{
+                    default: true,
+                }, {
+                    type: "confirm",
+                    name: 'haveDemo',
+                    message: '生成的项目是否涵盖管理工程Demo用例 Y || N',
+                    default: false,
+                }, {
                     name: 'version',
                     message: '请输入项目版本',
-                    default:"1.0.0",
+                    default: "1.0.0",
                 }
             ]).then((answers) => {
-                const spinner = ora('正在下载模板...');
+                const spinner = ora('正在生成模板...');
                 spinner.start();
-                download('gitlwz/new-quant', name, (err) => {
-                    if(err){
+                let url = "direct:http://gitlab.quantdo.cn/liuwz/quant-template.git";
+                if (!!answers.haveDemo) {
+                    url = "direct:http://gitlab.quantdo.cn/liuwz/quant-template.git#demo";
+                }
+                download(url, name, { clone: true }, (err) => {
+                    if (err) {
                         spinner.fail();
-                        console.log(symbols.error, chalk.red(err),err);
-                    }else{
+                        console.log(symbols.error, chalk.red(err), err);
+                    } else {
                         spinner.succeed();
                         const fileName = `${name}/package.json`;
                         const configName = `${name}/src/common/config.js`;
@@ -54,15 +63,15 @@ program.version('1.0.0', '-v, --version')
                             version: answers.version
                         }
                         const config = {
-                            isTop:answers.isTop,
+                            isTop: answers.isTop,
                             title: answers.title,
                         }
-                        if(fs.existsSync(fileName)){
+                        if (fs.existsSync(fileName)) {
                             const content = fs.readFileSync(fileName).toString();
                             const result = handlebars.compile(content)(meta);
                             fs.writeFileSync(fileName, result);
                         }
-                        if(fs.existsSync(configName)){
+                        if (fs.existsSync(configName)) {
                             const content = fs.readFileSync(configName).toString();
                             const result = handlebars.compile(content)(config);
                             fs.writeFileSync(configName, result);
@@ -71,7 +80,7 @@ program.version('1.0.0', '-v, --version')
                     }
                 })
             })
-        }else{
+        } else {
             // 错误提示项目已存在，避免覆盖原有项目
             console.log(symbols.error, chalk.red('项目已存在'));
         }
